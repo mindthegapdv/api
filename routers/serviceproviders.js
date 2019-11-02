af648da089f3cd4101194fb41ea39f0fe2a6e35a
Matthew Brown <me@matthewbrown.io>
2019-11-02 19:23:03 -0400

    Add service providers creat endpoint

--- routers/serviceproviders.js
+++ routers/serviceproviders.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { celebrate, Joi } = require('celebrate');
 const { asyncify } = require('../utils');
 // const { requireAuth } = require('../middleware');
 const ServiceProvider = require('../models/serviceproviders');
@@ -9,6 +10,19 @@ const createServiceProviderRouter = () => {
   router.get('/', asyncify(async (req, res) => {
     const providers = await ServiceProvider.findAll();
     return res.json(providers);
+  }));
+
+  router.post('/', celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      contactEmail: Joi.string().required(),
+      noticePeriod: Joi.number(),
+      costPerPerson: Joi.number().required(),
+    }),
+  }), asyncify(async (req, res) => {
+    const provider = await ServiceProvider.build(req.body);
+    const result = await provider.save();
+    res.json(result);
   }));
   return router;
 };
